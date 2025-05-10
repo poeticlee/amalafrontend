4087e8ca176f2734057e8a14a4db7ed9cbec07c2
code-forcer <135433507+code-forcer@users.noreply.github.com>
2025-05-10 16:55:36 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Hero from "./components/hero";
-import About from "./components/about";
-import Skills from "./components/skills";
-import Projects from "./components/projects";
-import Contact from "./components/contact";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 import Footer from "./components/footer";
-import Header from "./components/header";
+import Header from "./components/Header";
 
 
 function App() {
